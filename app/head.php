b4a57e9d705e2932940b93af26b82948d85cba79
Maciej Olszewski <hey@maciejolszewski.com>
2017-04-25 21:55:41 +0100

    added logout button - not styled yet...

---
+++ app/head.php
@@ -0,0 +1,20 @@
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Where is ?</title>
+
+
+    <!--build:css css/styles.min.css-->
+    <link rel="stylesheet" href="./scss/lib/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css" media="screen" title="no title">
+    <!--endbuild-->
+
+    <!--build:js js/main.min.js -->
+    <script type="text/javascript" src="./js/lib/bootstrap.js"></script>
+    <script src="js/test.js"></script>
+    <!-- endbuild -->
+
+
+
+
+  </head>
